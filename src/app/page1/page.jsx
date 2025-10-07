5d2189f3fe86dd12a7f726f667c6109a4723a133
Beatriz L <158210831+limabea23@users.noreply.github.com>
2025-10-07 10:59:36 -0300

    first commit

---
+++ src/app/page1/page.jsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useEffect, useState, useRef } from "react";
+import styles from "./page1.module.css";
+
+const TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
+
+export default function Geolocalizacao() {
+    const [localizacao, setLocalizacao] = useState(null);
+    const mapRef = useRef(null);
+
+    useEffect(() => {
+        const link = document.createElement("link");
+        link.href = "https://api.mapbox.com/mapbox-gl-js/v3.0.0/mapbox-gl.css";
+        link.rel = "stylesheet";
+        document.head.appendChild(link);
+
+        const script = document.createElement("script");
+        script.href = "https://api.mapbox.com/mapbox-gl-js/v3.0.0/mapbox-gl.css";
+        script.onload = () => {
+            navigator.geolocation.getCurrentPosition((pos) => {
+                const coords = [pos.coords.longitude, pos.coords.latitude];
+                setLocalizacao(coords);
+
+                mapboxgl.accessToken = TOKEN;
+                const map = new mapboxgl.Map({
+                    container: mapRef.current,
+                    style:"mapbox://styles/mapbox/streets-v12",
+                    center: coords,
+                    zoom: 14,
+                });
+
+                new mapboxgl.Marker({ color: "#FFF000"})
+                .setLngLat(coords)
+                .setPopup(
+                    new mapboxgl.Popup().setHTML(
+                        "<stonger>Você está aqui!</strong>"
+                    )
+                )
+                .addTo(map);
+            },
+            () => alert("Erro ao obter localização")
+        );
+        };
+        document.body.appendChild(script);
+    }, []);
+
+    return (
+        <div className={styles.container}>
+            <div ref={mapRef} className={styles.map}>
+                {localizacao && (
+                    <div className={styles.info}>
+                        <h2>Localização Atual</h2>
+                        <p>Longitude: {localizacao[0].toFixed(2)}</p>
+                        <p>Latitude: {localizacao[1].toFixed(2)}</p>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+}
